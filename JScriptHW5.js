8290a21957ba4c72fc13f632dea4d105c6b19e1c
Joyce Figueroa <ondestajoyce@yahoo.com>
2013-03-18 00:18:22 -0700

    testing || again.

--- JScriptHW5.js
+++ JScriptHW5.js
@@ -6,7 +6,7 @@ var number = prompt('Please enter a number between 2 and 10: ');
     document.write('</h2>');
     }
 
-if (number <= 1 || number >=11) {
+if (number <= 1 || number >= 11) {
 document.write('<h2 style="color:red";>');
 document.write('Your input is' + ' ' + number + '.' + 'The valid input number is between 2 and 10. Please reload this page and try again.' + '</h2>');
 }
