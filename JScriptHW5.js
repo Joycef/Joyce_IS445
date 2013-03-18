e84568bdd62ff8cf9787ec1bd8803c96a2f053e9
Joyce Figueroa <ondestajoyce@yahoo.com>
2013-03-18 00:31:28 -0700

    changes to task 2; continued.

--- JScriptHW5.js
+++ JScriptHW5.js
@@ -4,7 +4,7 @@ var number = prompt('Please enter a number between 2 and 10: ');
 
     document.write('<h2>'); document.write('Your input number is' + ' ' + number + '.');
     document.write('</h2>');
-    }
+    
 
 if (number <= 1 || number >= 11) {
 document.write('<h2 style="color:red";>');
@@ -15,11 +15,11 @@ var q = number;
 
 var counter = 0;
 
-while (q >= 0.000001) {
-    q /= 2;
+do {q /= 2;
     counter++;
-    }
-
-    {document.write('<h2>');document.write('The number of times to divide' + ' ' + number + ' ' + 'by 2 to get a value less than one millionth is' + ' ' + counter + '.');
+    document.write('<h2>');document.write('The number of times to divide' + ' ' + number + ' ' + 'by 2 to get a value less than one millionth is' + ' ' + counter + '.');
     document.write('<h2>');
     }
+while (q >= 0.000001);
+
+   
