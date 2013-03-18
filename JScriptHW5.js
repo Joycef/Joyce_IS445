d6b69640b07cf4ee3a3ca5285326c45e134fbc53
Joyce Figueroa <ondestajoyce@yahoo.com>
2013-03-17 23:36:50 -0700

    fixing task 2.

--- JScriptHW5.js
+++ JScriptHW5.js
@@ -9,12 +9,15 @@ if (number <= 1 && number >=11) {
 document.write('<h2 style="color:red";>');
 document.write('Your input is' + ' ' + number + '.' + 'The valid input number is between 2 and 10. Please reload this page and try again.' + '</h2>');
 }
+var quotient;
 var count;
 
-while (number >= 0.000001) {
-    number /= 2; 
+quotient = number/2
+
+while (quotient >= 0.000001) {
+    quotient =/ 2; 
     count = 0.00;
     }
-    {document.write('<h2>');document.write('The number of times to divide' + ' ' + number + ' ' + 'by 2 to get a value less than one millionth is' + count);
+    {document.write('<h2>');document.write('The number of times to divide' + ' ' + number + ' ' + 'by 2 to get a value less than one millionth is' + ' ' + count);
     document.write('<h2>');
     }
