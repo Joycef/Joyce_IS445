d4aedb7badbb327ce1ae95d4ef297bf840d74c13
Joyce Figueroa <ondestajoyce@yahoo.com>
2013-03-18 00:51:12 -0700

    continued loop issues.

--- JScriptHW5.js
+++ JScriptHW5.js
@@ -2,7 +2,8 @@
 var number = prompt('Please enter a number between 2 and 10: ');
 
 
-    document.write('<h2>'); document.write('Your input number is' + ' ' + number + '.');
+    document.write('<h2>'); 
+    document.write('Your input number is' + ' ' + number + '.');
     document.write('</h2>');
     
 
@@ -18,5 +19,4 @@ while (q >= 0.000001) {
     q /= 2;
     counter++;
     document.write('<h2>');document.write('The number of times to divide' + ' ' + number + ' ' + 'by 2 to get a value less than one millionth is' + ' ' + counter + '.');
-    document.write('<h2>');
-    }
+    document.write('<h2>');}
