5d8ab56265f1790a63f5efd07de14843877cfb81
Joyce Figueroa <ondestajoyce@yahoo.com>
2013-03-18 00:38:44 -0700

    loop changes.

--- JScriptHW5.js
+++ JScriptHW5.js
@@ -14,9 +14,10 @@ document.write('Your input is' + ' ' + number + '.' + 'The valid input number is
 var q = number;
 var counter = 0;
 
+while (q >= 0.000001)
 do {q /= 2;
     counter++;
     document.write('<h2>');document.write('The number of times to divide' + ' ' + number + ' ' + 'by 2 to get a value less than one millionth is' + ' ' + counter + '.');
     document.write('<h2>');
     }
-while (q >= 0.000001);
+
