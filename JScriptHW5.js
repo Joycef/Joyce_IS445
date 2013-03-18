7147d6afb2ddad5b6053bebea99a2d90e4ad780b
Joyce Figueroa <ondestajoyce@yahoo.com>
2013-03-18 00:01:23 -0700

    fixing task 2; times to be divided.

--- JScriptHW5.js
+++ JScriptHW5.js
@@ -9,15 +9,14 @@ if (number <= 1 && number >=11) {
 document.write('<h2 style="color:red";>');
 document.write('Your input is' + ' ' + number + '.' + 'The valid input number is between 2 and 10. Please reload this page and try again.' + '</h2>');
 }
-var quotient;
-var count;
 
-quotient = number/2
-count = 0.00;
+var q = number;
 
-if (quotient >= 0.000001) {
-    quotient /= 2;
-    count++;
+var counter = 0;
+
+while (q >= 0.000001) {
+    q /= 2;
+    counter++;
     }
     {document.write('<h2>');document.write('The number of times to divide' + ' ' + number + ' ' + 'by 2 to get a value less than one millionth is' + ' ' + count + '.');
     document.write('<h2>');
