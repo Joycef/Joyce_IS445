5f329007558d4774ef19bc2ec2d8c4bf0c03be12
Joyce Figueroa <ondestajoyce@yahoo.com>
2013-03-17 23:19:34 -0700

    Edited js and html files after viewing webpage in browser.

--- JScriptHW5.js
+++ JScriptHW5.js
@@ -1,4 +1,5 @@
-﻿var number = prompt('Please enter a number between 2 and 10: ');
+﻿
+var number = prompt('Please enter a number between 2 and 10: ');
 
 
     document.write('<h2>'); document.write('Your input number is' + '' + number + '.');
@@ -6,7 +7,7 @@
 
 if (number <= 1 && number >=11) {
 document.write('<h2 style="color:red";>');
-document.write('Your input is' + number + '.' + 'The valid input number is between 2 and 10. Please reload this page and try again.' + '</h2>');
+document.write('Your input is' + ' ' + number + '.' + 'The valid input number is between 2 and 10. Please reload this page and try again.' + '</h2>');
 }
 x = number;
 var count;
@@ -15,6 +16,6 @@ while (x >= 0.000001) {
     x /= 2; 
     count = 0.00
     }
-    {document.write('<h2>');document.write('The number of times to divide + number + by 2 to get a value less than one millionth is + count');
+    {document.write('<h2>');document.write('The number of times to divide' + number + 'by 2 to get a value less than one millionth is' + count);
     document.write('<h2>');
     }
