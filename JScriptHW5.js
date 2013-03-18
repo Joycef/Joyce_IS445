95538455b1f08b1ab4a01363e9f0b8e30deab2d3
Joyce Figueroa <ondestajoyce@yahoo.com>
2013-03-17 23:28:20 -0700

    Nothing showed on webpage after last commit; attempting to fix this.

--- JScriptHW5.js
+++ JScriptHW5.js
@@ -7,7 +7,7 @@ var number = prompt('Please enter a number between 2 and 10: ');
 
 if (number <= 1 && number >=11) {
 document.write('<h2 style="color:red";>');
-document.write('Your input is' + ' ' + number + '.' + 'The valid input number is between 2 and 10. Please reload this page and try again.''</h2>');
+document.write('Your input is' + ' ' + number + '.' + 'The valid input number is between 2 and 10. Please reload this page and try again.' + '</h2>');
 }
 var count;
 
