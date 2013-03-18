206da57bffb3a0881fccf98eb2de35913c342e5e
Joyce Figueroa <ondestajoyce@yahoo.com>
2013-03-18 00:09:11 -0700

    changes again 2

--- JScriptHW5.js
+++ JScriptHW5.js
@@ -1,12 +1,12 @@
 ﻿
 var number = prompt('Please enter a number between 2 and 10: ');
 
-if (number > 2 && number < 10) {
+
     document.write('<h2>'); document.write('Your input number is' + ' ' + number + '.');
     document.write('</h2>');
     }
 
-else if (number <= 1 && number >=11) {
+if (number <= 1 && number >=11) {
 document.write('<h2 style="color:red";>');
 document.write('Your input is' + ' ' + number + '.' + 'The valid input number is between 2 and 10. Please reload this page and try again.' + '</h2>');
 }
@@ -19,6 +19,6 @@ while (q >= 0.000001) {
     q /= 2;
     counter++;
     }
-    {document.write('<h2>');document.write('The number of times to divide' + ' ' + number + ' ' + 'by 2 to get a value less than one millionth is' + ' ' + count + '.');
+    {document.write('<h2>');document.write('The number of times to divide' + ' ' + number + ' ' + 'by 2 to get a value less than one millionth is' + ' ' + counter + '.');
     document.write('<h2>');
     }
