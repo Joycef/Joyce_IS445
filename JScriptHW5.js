b636e65a61cfac42fc2d51c6d3ae04bef48e9a68
Joyce Figueroa <ondestajoyce@yahoo.com>
2013-03-17 23:43:41 -0700

    Fixing task 2 again.

--- JScriptHW5.js
+++ JScriptHW5.js
@@ -2,7 +2,7 @@
 var number = prompt('Please enter a number between 2 and 10: ');
 
 
-    document.write('<h2>'); document.write('Your input number is' + '' + number + '.');
+    document.write('<h2>'); document.write('Your input number is' + ' ' + number + '.');
     document.write('</h2>');
 
 if (number <= 1 && number >=11) {
@@ -13,11 +13,12 @@ var quotient;
 var count;
 
 quotient = number/2
+count = 0.00;
 
 if (quotient >= 0.000001) {
-    quotient /= 2; 
-    count = 0.00;
+    quotient /= 2;
+    count++;
     }
-    {document.write('<h2>');document.write('The number of times to divide' + ' ' + number + ' ' + 'by 2 to get a value less than one millionth is' + ' ' + count);
+    {document.write('<h2>');document.write('The number of times to divide' + ' ' + number + ' ' + 'by 2 to get a value less than one millionth is' + ' ' + count + '.');
     document.write('<h2>');
     }
